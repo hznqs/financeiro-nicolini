bd8664b265fe1998e6676cd1bae30134daa5233c
hznqs <caio.nicolini09@gmail.com>
2026-05-07 20:42:38 -0300

    foi talvez

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
-    tanstackStart({
-      server: {
-        entry: "src/server.ts",
-      },
-    }),
-    react(),
+    tanstackStart(),
     tailwindcss(),
     tsconfigPaths(),
-    // Cloudflare plugin for build
-    cloudflare(),
   ],
 });
